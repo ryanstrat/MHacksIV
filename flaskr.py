4f09cfd72701f05094a3093f57b669339bf9fb94
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-09-07 08:12:56 -0400

    fsidfh

--- flaskr.py
+++ flaskr.py
@@ -3,10 +3,16 @@ from flask import request
 from predict import getGoogleData
 from predict import getFBMSG
 from predict import pred
+import bloomberg
 import json
 from flask import render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+
+def alterMonies(adjustment):
+    
+
 
 @app.route('/')
 def index():
@@ -19,8 +25,17 @@ def sentiment():
     print googpred
     fbpred=pred(getFBMSG(vars[1]))
     print fbpred
-    jso=json.dumps({"google":googpred,"facebook":fbpred})
-    return jso
+    
+    vti = bloomberg.getPrice("VTI US EQUITY", "2014", "09", "05")
+    bnd = bloomberg.getPrice("BND US EQUITY", "2014", "09", "05")
+    
+    googpred = 2 * (googpred - 0.5)
+    fbpred = 2 * (fbpred - 0.5)
+    
+    turmoil = googpred + fbpred;
+    
+    jso=json.dumps({"turmoil":turmoil, "VTI":vti, "BND":bnd})
+    return render_template('dashboard.html', vti=vti, bnd=bnd, turmoil=tirmoil)
 
 
 if __name__ == "__main__":
