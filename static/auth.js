439cdcb522855bf71db25b7d4afd8af5a64621a6
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-09-07 04:29:39 -0400

    Reorganized files for server

---
+++ static/auth.js
@@ -0,0 +1,47 @@
+var googleKey;
+var facebookKey;
+
+
+var ref = new Firebase("https://torid-heat-3815.firebaseio.com/");
+var authClient = new FirebaseSimpleLogin(ref, function(error, user) {
+    if (error) {
+        console.log(error);
+    } else if (user) {
+        console.log("User ID: " + user.uid + ", Provider: " + user.provider);
+        if(authClient.provider == 'google') {
+            googleKey = user.accessToken;
+            if(isnull(facebookKey)) {
+                authFacebook();
+            }
+        } else {
+            facebookKey = user.accessToken;
+            if(isnull(googleKey)) {
+                authGoogle();
+            }
+        }
+    } else {
+        
+    }
+});
+
+function authGoogle(){
+    authClient.login('google');
+
+}
+
+function authFacebook(){
+    authClient.login('facebook');
+
+}
+
+function loggedIn(){
+    var url = sprintf("http://centiment.me/dashboard?google=%s&facebook=%s", googleKey, facebookKey);
+    window.location.replace(url);
+}
+
+$(document).ready(function(){
+    console.log("signing in");
+    $("LogIn").click(function(){
+        authGoogle();
+    });
+});
